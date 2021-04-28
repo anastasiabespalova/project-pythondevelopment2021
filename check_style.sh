d97fa14b1360627596ce397da360d58d50488713
Анастасия Беспалова <anastasiabespalova@192.168.1.6>
2021-04-28 22:13:30 +0300

    Check style.

---
+++ check_style.sh
@@ -0,0 +1,8 @@
+pydocstyle download_saved_models.py  
+pydocstyle main.py
+pydocstyle transformer_net.py
+pydocstyle utils.py
+pylint download_saved_models.py  
+pylint main.py
+pylint transformer_net.py
+pylint utils.py
